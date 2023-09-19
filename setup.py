6121e6528ffd19abdc5f64dd5c3954228bc5ad53
sanchda <sanchda@users.noreply.github.com>
2023-09-19 13:14:22 +0000

    Just add cython

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, Extension, find_packages
 import subprocess
 
+from Cython.Build import cythonize
+import Cython.Distutils
+
 # Run the script
 subprocess.run(['sh', 'build_ddup.sh'])
 
 # Set the compilers
-module = Extension('miniprof.sampler',
+modules = cythonize(
+           [
+             Extension('miniprof.sampler',
                     sources = [
                         'miniprof/sampler.c',
                     ],
@@ -18,15 +23,23 @@ module = Extension('miniprof.sampler',
                     ],
                     libraries=['stdc++'],
                     extra_compile_args=['-std=c11', '-O3']
-                )
+             ),
+             Cython.Distutils.Extension(
+               "miniprof.profiler",
+               sources=["miniprof/_profiler.pyx"],
+               language="c",
+             ),
+           ]
+         )
 setup(name = 'miniprof',
       version = '0.3.2',
       description = 'A very small profiler',
-      ext_modules = [module],
+      ext_modules = modules,
       entry_points={
           "console_scripts": [
               "miniprof-run= miniprof.profiler:main",
           ],
       },
+    setup_requires=["setuptools_scm[toml]>=4", "cython", "cmake>=3.24.2; python_version>='3.6'"],
     packages=find_packages(),
 )
